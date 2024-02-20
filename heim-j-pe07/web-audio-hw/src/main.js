e9d23b557b343476ec94453e0a9650b6669b4c39
jackson <jacksontheim@gmail.com>
2024-02-20 15:10:36 -0500

    dwdsd

--- heim-j-pe07/web-audio-hw/src/main.js
+++ heim-j-pe07/web-audio-hw/src/main.js
@@ -1,59 +1,109 @@
 /*
-	main.js is primarily responsible for hooking up the UI to the rest of the application 
-	and setting up the main event loop
+  main.js is primarily responsible for hooking up the UI to the rest of the application 
+  and setting up the main event loop
 */
 
 // We will write the functions in this file in the traditional ES5 way
 // In this instance, we feel the code is more readable if written this way
 // If you want to re-write these as ES6 arrow functions, to be consistent with the other files, go ahead!
 
+//imports
 import * as utils from './utils.js';
 import * as audio from './audio.js';
+import * as canvas from './visualizer.js';
+
+//params object
+const drawParams = {
+  showGradient: true,
+  showBars: true,
+  showCircles: false,
+  showNoise: true
+}
 
 // 1 - here we are faking an enumeration
 const DEFAULTS = Object.freeze({
-	sound1  :  "media/New Adventure Theme.mp3"
+  sound1: "media/New Adventure Theme.mp3"
 });
 
-audio.setupWebaudio(DEFAULTS.sound1);
+function init() {
+  audio.setupWebaudio(DEFAULTS.sound1);
+  console.log("init called");
+  console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
+  let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
+  setupUI(canvasElement);
 
-function init(){
-	console.log("init called");
-	console.log(`Testing utils.getRandomColor() import: ${utils.getRandomColor()}`);
-	let canvasElement = document.querySelector("canvas"); // hookup <canvas> element
-	setupUI(canvasElement);
+  canvas.setupCanvas(canvasElement, audio.analyserNode);
+
+  loop();
 }
 
-function setupUI(canvasElement){
+function setupUI(canvasElement) {
   // A - hookup fullscreen button
   const fsButton = document.querySelector("#fsButton");
-	
+
   // add .onclick event to button
   fsButton.onclick = e => {
     console.log("goFullscreen() called");
     utils.goFullscreen(canvasElement);
   };
 
-  //play button
-  playButton.onclick = e =>{
+  //PLAY BUTTON
+  playButton.onclick = e => {
     console.log(`audioCtx.state before = ${audio.audioCtx.state}`)
-  }
 
-  //check if in suspend state (autoplay)
-  if(audio.audioCtx.state == "suspended"){
-    audio.audioCtx.resume();
-  }
-  console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
-  if(e.target.dataset.playing = "no"){
-    //if currently paused, play it
-    audio.playCurrentSound();
-    e.target.dataset.playing = "yes";
 
-  }else{
-    audio.pauseCurrentSound();
-    e.target.dataset.playing = "no";
-  }
-	
+    //check if in suspend state (autoplay)
+    if (audio.audioCtx.state == "suspended") {
+      audio.audioCtx.resume();
+    }
+    console.log(`audioCtx.state after = ${audio.audioCtx.state}`);
+    if (e.target.dataset.playing == "no") {
+      //if currently paused, play it
+      audio.playCurrentSound();
+      e.target.dataset.playing = "yes";
+
+    } else {
+      audio.pauseCurrentSound();
+      e.target.dataset.playing = "no";
+    }
+
+  };
+
+  //VOLUME SLIDER
+  //get references to them
+  let volumeSlider = document.querySelector("#volumeSlider");
+  let volumeLabel = document.querySelector("#volumeLabel");
+
+  //change on input
+  volumeSlider.oninput = e => {
+    //set gain
+    audio.setVolume(e.target.value);
+    //update value on label
+    volumeLabel.innerHTML = Math.round((e.target.value / 2 * 100));
+  };
+
+  //set initial
+  volumeSlider.dispatchEvent(new Event("input"));
+
+  //TRACK SELECT
+  let trackSelect = document.querySelector("#trackSelect");
+  //onchange event
+  trackSelect.onchange = e => {
+    audio.loadSoundFile(e.target.value);
+    //pause current if playing
+    if (playButton.dataset.playing == "yes") {
+      playButton.dispatchEvent(new MouseEvent("click"));
+    }
+  };
+
 } // end setupUI
 
-export {init};
+//DATA VISULIZE
+function loop() {
+  canvas.draw(drawParams);
+  requestAnimationFrame(loop);
+
+
+}
+
+export { init };
---
+++ heim-j-pe07/web-audio-hw/src/visualizer.js
@@ -0,0 +1,100 @@
+/*
+    The purpose of this file is to take in the analyser node and a <canvas> element: 
+      - the module will create a drawing context that points at the <canvas> 
+      - it will store the reference to the analyser node
+      - in draw(), it will loop through the data in the analyser node
+      - and then draw something representative on the canvas
+      - maybe a better name for this file/module would be *visualizer.js* ?
+*/
+
+import * as utils from './utils.js';
+
+let ctx, canvasWidth, canvasHeight, gradient, analyserNode, audioData;
+
+
+function setupCanvas(canvasElement, analyserNodeRef) {
+    // create drawing context
+    ctx = canvasElement.getContext("2d");
+    canvasWidth = canvasElement.width;
+    canvasHeight = canvasElement.height;
+    // create a gradient that runs top to bottom
+    gradient = utils.getLinearGradient(ctx, 0, 0, 0, canvasHeight, [{ percent: 0, color: "blue" }, { percent: .25, color: "green" }, { percent: .5, color: "yellow" }, { percent: .75, color: "red" }, { percent: 1, color: "magenta" }]);
+    // keep a reference to the analyser node
+    analyserNode = analyserNodeRef;
+    // this is the array where the analyser data will be stored
+    audioData = new Uint8Array(analyserNode.fftSize / 2);
+}
+
+function draw(params = {}) {
+    // 1 - populate the audioData array with the frequency data from the analyserNode
+    // notice these arrays are passed "by reference" 
+    //analyserNode.getByteFrequesncyData(audioData);
+    // OR
+    analyserNode.getByteTimeDomainData(audioData); // waveform data
+
+    // 2 - draw background
+    ctx.save();
+    ctx.fillStyle = "black";
+    ctx.globalAlpha = .1;
+    ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+    ctx.restore();
+
+    // 3 - draw gradient
+    if (params.showGradient) {
+        ctx.save();
+        ctx.fillStyle = gradient;
+        ctx.globalAlpha = .3;
+        ctx.fillRect(0, 0, canvasWidth, canvasHeight);
+        ctx.restore;
+    }
+
+    // 4 - draw bars
+    if (params.showBars) {
+        let barSpacing = 4;
+        let margin = 5;
+        let screenWidthForBars = canvasWidth - (audioData.length * barSpacing) - margin * 2;
+        let barWidth = screenWidthForBars / audioData.length;
+        let barHeight = 200;
+        let topSpacing = 100;
+
+        ctx.save();
+        ctx.fillStyle = `rgb(255, 255, 255, 0.5)`
+        //loop througbh data
+        for (let i = 0; i < audioData.length; i++) {
+            ctx.fillRect(margin + i * (barWidth + barSpacing), topSpacing + 256 - audioData[i], barWidth, barHeight);
+            ctx.strokeRect(margin + i * (barWidth + barSpacing), topSpacing + 256 - audioData[i], barWidth, barHeight);
+        }
+        ctx.restore();
+    }
+
+    // 5 - draw circles
+    if (params.showCircles) {
+        let maxRadius = canvasHeight / 4;
+        ctx.save();
+        ctx.globalAlpha = 0.5;
+        for (let i = 0; i, audioData.length; i++) {
+
+            let percent = audioData[i] / 255;
+            let circleRadius = percent * maxRadius;
+
+            //redish circls
+            ctx.fillStyle = utils.makeColor(255, 111, 111, .34 - percent / 3);
+            ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius, 0, 2 * Math.PI, false);
+
+            //bluish circles
+            ctx.fillStyle = utils.makeColor(0, 0, 225, .10 - percent / 10);
+            ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius * 1.5, 0, 2 * Math.PI, false);
+
+            //yellowish circles
+            ctx.fillStyle = utils.makeColor(200, 200, 0, .5 - percent / 5);
+            ctx.arc(canvasWidth / 2, canvasHeight / 2, circleRadius * .5, 0, 2 * Math.PI, false);
+
+        }
+        ctx.fill();
+        ctx.closePath();
+        ctx.restore();
+    }
+
+}
+
+export { setupCanvas, draw };
